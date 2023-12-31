c2dbc22561a7d3147dec58594d50d2e08b97afa7
quang-kingfood <quang.ngovan@kingfoodmart.net>
2023-12-31 19:54:43 +0700

    upload

--- main.js
+++ main.js
@@ -1,61 +1,53 @@
-const QR_URL = "https://qr.sepay.vn/img?acc=8154461&bank=970416";
-$("#qrImage").attr("src", QR_URL);
+$("#qrImage").attr("src", 'https://qr.sepay.vn/img?acc=8154461&bank=970416');
+$('#dialog').modal('show')
 
-$(document).ready(function() {
-    setupListeners();
-    $('#dialog').modal('show');
+$('#start').on('click', () => {
+    let value_input = $('#value_start').val()
+    console.log(value_input.length)
+    if(value_input.length > 0) {
+        $('#dialog').modal('hide')
+        $("#banking_amount").focus();
+        updateQRCode()
+    } else {
+        alert("Please enter information")
+    }
+})
+
+$("#value_start").on("input", function () {
+    if($('#value_start').val().length > 0) {
+        $('#start').prop( "disabled", false );
+        $('#banking_content').val($('#value_start').val())
+    }else {
+        $('#start').prop( "disabled", true );
+    }
 });
 
-function setupListeners() {
-    $('#start').on('click', handleStartClick);
-    $("#value_start").on("input", handleValueInputChange);
-    $("#banking_amount, #banking_content").on("input", updateQRCode);
-    $("#banking_amount").on("input", handleAmountInput);
-    
-    $("#banking_copy, #banking_id_copy, #banking_name_copy, #banking_content_copy, #banking_amount_copy").click(handleCopyClick);
-}
+$("#banking_amount, #banking_content").on("input", function () {
+    updateQRCode();
+});
 
-function handleStartClick() {
-    const value_input = $('#value_start').val();
-    if (value_input.length > 0) {
-        $('#dialog').modal('hide');
-        $("#banking_amount").focus();
-        updateQRCode();
-    } else {
-        alert("Please enter information");
-    }
-}
-
-function handleValueInputChange() {
-    const value = $('#value_start').val();
-    if (value.length > 0) {
-        $('#start').prop("disabled", false);
-        $('#banking_content').val(value);
-    } else {
-        $('#start').prop("disabled", true);
-    }
-}
-
-function handleAmountInput() {
+$("#banking_amount").on("input", function () {
     this.value = this.value.replace(/[^0-9]/g, '');
     updateQRCode();
-}
+});
 
-function handleCopyClick() {
-    const inputId = $(this).data('input-id');
-    copyToClipboard(inputId);
-}
+$("#banking_copy").click(function () { copyToClipboard("banking"); });
+$("#banking_id_copy").click(function () { copyToClipboard("banking_id"); });
+$("#banking_name_copy").click(function () { copyToClipboard("banking_name"); });
+$("#banking_content_copy").click(function () { copyToClipboard("banking_content"); });
+$("#banking_amount_copy").click(function () { copyToClipboard("banking_amount"); });
 
 function copyToClipboard(inputId) {
-    const input = $("#" + inputId)[0];
+    const input = $("#" + inputId)[ 0 ];
     input.select();
     document.execCommand('copy');
-    alert(`Đã sao chép ! \n\n${input.value}`);
+
+    alert(`Copy success ! \n\n${input.value}`);
 }
 
 function updateQRCode() {
     const amount = $("#banking_amount").val();
     const des = $("#banking_content").val();
-    const newSrc = `${QR_URL}&amount=${amount}&des=${des}`;
+    const newSrc = `https://qr.sepay.vn/img?acc=8154461&bank=970416&amount=${amount}&des=${des}`;
     $("#qrImage").attr("src", newSrc);
 }
